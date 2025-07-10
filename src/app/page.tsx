978efd11599b6f784932fa59d9f002e9f7f56bad
cakfan <taufan.fhm8@gmail.com>
2025-07-10 17:19:06 +0700

    fix: some error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getMe } from "@/actions/user";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Home() {
   const me = await getMe();
@@ -30,11 +31,9 @@ export default async function Home() {
         </ol>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
+          <Link
             className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/signin"
           >
             <Image
               className="dark:invert"
@@ -43,8 +42,8 @@ export default async function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            Sign In
+          </Link>
           <a
             className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
--- src/middleware.ts
+++ src/middleware.ts
@@ -11,19 +11,21 @@ import {
 export async function middleware(request: NextRequest) {
   const session = getSessionCookie(request);
 
-  const isApiAuth = request.nextUrl.pathname.startsWith(apiAuthPrefix);
+  const { pathname } = request.nextUrl;
 
-  const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
+  const isApiAuth = pathname.startsWith(apiAuthPrefix);
 
-  const isAuthRoute = () => {
-    return authRoutes.some((path) => request.nextUrl.pathname.startsWith(path));
-  };
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route),
+  );
+
+  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
 
   if (isApiAuth) {
     return NextResponse.next();
   }
 
-  if (isAuthRoute()) {
+  if (isAuthRoute) {
     if (session) {
       return NextResponse.redirect(
         new URL(DEFAULT_LOGIN_REDIRECT, request.url),
@@ -41,13 +43,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
